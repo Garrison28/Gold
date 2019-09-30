27cd0d6016de2326d8cd4ec0ccc7aa07dc6d2ce6
Garrison28 <ghighsmith28@gmail.com>
2019-09-30 16:35:41 -0700

    game at MVP

--- js/functions.js
+++ js/functions.js
@@ -3,65 +3,68 @@ function reset() {
     enemyOne = Object.assign({}, enemieOneStart)
     enemyTwo = Object.assign({}, enemieTwoStart)
     goldenO = Object.assign({}, goldObjStart)
+    goldenOTwo = Object.assign({}, goldenObjTwoStart)
+    goldenOThree = Object.assign({}, goldenObjThreeStart)
+    goldenOFour = Object.assign({}, goldenObjFourStart)
 }
 
-function startGame() {
-    showDisplay();
-}
+var loop;
+document.addEventListener('keydown', allMovement)
+// initilize the game setup
+function gameInit() {
+    loseDisplay.style.display = 'none'
+    winDisplay.style.display = 'none'
+    //start the gameloop for the game board
+    loop = setInterval(gameLoop, 60)
+    function gameLoop() {
+        ctx.clearRect(0, 0, game.width, game.height)
+        if (enemyOne.alive) {
+            enemyOne.render()
+            detectHit()
+            enemyOneMovement()
+        }
+        if (enemyTwo.alive) {
+            enemyTwo.render()
+            detectHit()
+            enemyTwoMovement()
+        }
+        if (enemyThree.alive) {
+            enemyThree.render()
+            detectHit()
+            enemyThreeMovement()
+        }
+        if (enemyFour.alive) {
+            enemyFour.render()
+            detectHit()
+            enemyFourMovement()
+        }
+        if (goldenO.alive) {
+            goldenO.render()
+            detectHit()
+        }
+        if (goldenOTwo.alive) {
+            goldenOTwo.render()
+            detectHit()
+        }
+        if (goldenOThree.alive) {
+            goldenOThree.render()
+            detectHit()
+        }
+        if (goldenOFour.alive) {
+            goldenOFour.render()
+            detectHit()
+        }
+        if (player.alive) {
+            player.render()
+            detectHit()
 
-function showDisplay(display) {
-    if (display === startDisplay) {
-        startDisplay.style.display = "block";
-        gameDisplay.style.display = "none";
-        loseDisplay.style.display = "none";
-    } else if (display === gameDisplay) {
-        startDisplay.style.display = "none";
-        gameDisplay.style.display = "block";
-        loseDisplay.style.display = "none";
-    } else if (display === loseDisplay) {
-        startDisplay.style.display = "none";
-        gameDisplay.style.display = "none";
-        loseDisplay.style.display = "block";
+        }
+        checkGame()
     }
-}
-
-function detectHit() {
-    // if (player.x <= enemyOne.x + enemyOne.width
-    //     && player.x + player.width >= enemyOne.x
-    //     && player.y <= enemyOne.y + enemyOne.height
-    //     && player.y + player.height >= enemyOne.y) {
-    //         enemyOne.alive = false
-    //     }
-    // if (player.x <= enemyTwo.x + enemyTwo.width
-    //     && player.x + player.width >= enemyTwo.x
-    //     && player.y <= enemyTwo.y + enemyTwo.height
-    //     && player.y + player.height >= enemyTwo.y){
-    //         enemyTwo.alive = false
-    //     }
-    if (player.x <= goldenO.x + goldenO.width
-        && player.x + player.width >= goldenO.x
-        && player.y <= goldenO.y + goldenO.height
-        && player.y + player.height > goldenO.y) {
-        goldenO.alive = false
-        gameWon()
-
-    }
-    if (enemyOne.x <= player.x + player.width
-        && enemyOne.x + enemyOne.width >= player.x
-        && enemyOne.y <= player.y + player.height
-        && enemyOne.y + enemyOne.height >= player.y) {
-        player.alive = false
-        gameLost()
-    }
-    if (enemyTwo.x <= player.x + player.width
-        && enemyTwo.x + enemyTwo.width >= player.x
-        && enemyTwo.y <= player.y + player.height
-        && enemyTwo.y + enemyTwo.height >= player.y) {
-        player.alive = false
-        gameLost()
-    }
-
-}
+    detectHit()
+    enemyOneMovement()
+    enemyTwoMovement()
+};
 
 function allMovement(e) {
     switch (e.keyCode) {
@@ -88,7 +91,82 @@ function allMovement(e) {
     }
 }
 
+function detectHit() {
+    // if (player.x <= enemyOne.x + enemyOne.width
+    //     && player.x + player.width >= enemyOne.x
+    //     && player.y <= enemyOne.y + enemyOne.height
+    //     && player.y + player.height >= enemyOne.y) {
+    //         enemyOne.alive = false
+    //     }
+    // if (player.x <= enemyTwo.x + enemyTwo.width
+    //     && player.x + player.width >= enemyTwo.x
+    //     && player.y <= enemyTwo.y + enemyTwo.height
+    //     && player.y + player.height >= enemyTwo.y){
+    //         enemyTwo.alive = false
+    //     }
+    if (player.x <= goldenO.x + goldenO.width
+        && player.x + player.width >= goldenO.x
+        && player.y <= goldenO.y + goldenO.height
+        && player.y + player.height > goldenO.y) {
+        goldenO.alive = false
+    }
+    if (player.x <= goldenOTwo.x + goldenOTwo.width
+        && player.x + player.width >= goldenOTwo.x
+        && player.y <= goldenOTwo.y + goldenOTwo.height
+        && player.y + player.height > goldenOTwo.y) {
+        goldenOTwo.alive = false
+    }
+    if (player.x <= goldenOThree.x + goldenOThree.width
+        && player.x + player.width >= goldenOThree.x
+        && player.y <= goldenOThree.y + goldenOThree.height
+        && player.y + player.height > goldenOThree.y) {
+        goldenOThree.alive = false
+    }
+    if (player.x <= goldenOFour.x + goldenOFour.width
+        && player.x + player.width >= goldenOFour.x
+        && player.y <= goldenOFour.y + goldenOFour.height
+        && player.y + player.height > goldenOFour.y) {
+        goldenOFour.alive = false
+    }
+    if (enemyOne.x <= player.x + player.width
+        && enemyOne.x + enemyOne.width >= player.x
+        && enemyOne.y <= player.y + player.height
+        && enemyOne.y + enemyOne.height >= player.y) {
+        player.alive = false
+    }
+    if (enemyTwo.x <= player.x + player.width
+        && enemyTwo.x + enemyTwo.width >= player.x
+        && enemyTwo.y <= player.y + player.height
+        && enemyTwo.y + enemyTwo.height >= player.y) {
+        player.alive = false
+    }
+    if (enemyThree.x <= player.x + player.width
+        && enemyThree.x + enemyThree.width >= player.x
+        && enemyThree.y <= player.y + player.height
+        && enemyThree.y + enemyThree.height >= player.y) {
+        player.alive = false
+
+    }
+    if (enemyFour.x <= player.x + player.width
+        && enemyFour.x + enemyFour.width >= player.x
+        && enemyFour.y <= player.y + player.height
+        && enemyFour.y + enemyFour.height >= player.y) {
+        player.alive = false
+    }
+}
+
 function enemyOneMovement() {
+
+    // if (enemyOne.x > game.width - 25) {
+    //     enemyOne.xSpeed *= -1;
+    // } else if (enemyOne.x < 1) {
+    //     enemyOne.xSpeed *= -1;
+    // } else if (enemyOne.y < 1) {
+    //     enemyOne.ySpeed *= -1;
+    // } else if (enemyOne.y > game.height - 25) {
+    //     enemyOne.ySpeed *= -1;
+    // }
+
 
     if (enemyOne.x < 0 || enemyOne.x > game.width - enemyOne.width) {
         enemyOne.velocity *= -1
@@ -97,15 +175,64 @@ function enemyOneMovement() {
 }
 
 function enemyTwoMovement() {
+    // if (enemyTwo.x > game.width - 25) {
+    //     enemyTwo.xSpeed *= -1;
+    // } else if (enemyTwo.x < 1) {
+    //     enemyTwo.xSpeed *= -1;
+    // } else if (enemyTwo.y < 1) {
+    //     enemyTwo.ySpeed *= -1;
+    // } else if (enemyTwo.y > game.height - 100) {
+    //     enemyTwo.ySpeed *= -1;
+    // }
+
     if (enemyTwo.y < 0 || enemyTwo.y > game.height - enemyTwo.height) {
         enemyTwo.velocity *= -1
     }
     enemyTwo.y += enemyTwo.velocity
 }
 
+function enemyThreeMovement() {
+
+    // if (enemyOne.x > game.width - 25) {
+    //     enemyOne.xSpeed *= -1;
+    // } else if (enemyOne.x < 1) {
+    //     enemyOne.xSpeed *= -1;
+    // } else if (enemyOne.y < 1) {
+    //     enemyOne.ySpeed *= -1;
+    // } else if (enemyOne.y > game.height - 25) {
+    //     enemyOne.ySpeed *= -1;
+    // }
+
+
+    if (enemyThree.x < 0 || enemyThree.x > game.width - enemyThree.width) {
+        enemyThree.velocity *= -1
+    }
+    enemyThree.x += enemyThree.velocity
+}
+
+function enemyFourMovement() {
+
+    // if (enemyOne.x > game.width - 25) {
+    //     enemyOne.xSpeed *= -1;
+    // } else if (enemyOne.x < 1) {
+    //     enemyOne.xSpeed *= -1;
+    // } else if (enemyOne.y < 1) {
+    //     enemyOne.ySpeed *= -1;
+    // } else if (enemyOne.y > game.height - 25) {
+    //     enemyOne.ySpeed *= -1;
+    // }
+
+
+    if (enemyFour.y < 0 || enemyFour.y > game.height - enemyFour.height) {
+        enemyFour.velocity *= -1
+    }
+    enemyFour.y += enemyFour.velocity
+}
+
 function gameWon() {
-    if (goldenO.alive === false) {
-        document.getElementById('gameDisplay').style.display = "none"
+    if (goldenO.alive === false && goldenOTwo.alive === false
+        && goldenOThree.alive === false && goldenOFour.alive === false) {
+        document.getElementById('gameDisplayOne').style.display = "none"
         document.getElementById('winDisplay').style.display = "block"
         console.log('you Win!')
         clearInterval(loop)
@@ -114,39 +241,17 @@ function gameWon() {
 
 function gameLost() {
     if (player.alive === false) {
-        document.getElementById('gameDisplay').style.display = "none"
+        document.getElementById('gameDisplayOne').style.display = "none"
         document.getElementById('loseDisplay').style.display = "block"
-        
+
         console.log("you Lose")
         clearInterval(loop)
     }
 }
 
-function gameLoop() {
-    ctx.clearRect(0, 0, game.width, game.height)
-    if (enemyOne.alive) {
-        enemyOne.render()
-        detectHit()
-        enemyOneMovement()
+function checkGame() {
+    if (player.alive === true) {
+        gameWon()
     }
-    if (enemyTwo.alive) {
-        enemyTwo.render()
-        detectHit()
-        enemyTwoMovement()
-    }
-    if (goldenO.alive) {
-        goldenO.render()
-        detectHit()
-    }
-    if (player.alive) {
-        player.render()
-        detectHit()
-
-    }
-
+    gameLost()
 }
-
-
-
-document.addEventListener('keydown', allMovement)
-var loop = setInterval(gameLoop, 60)
--- js/object.js
+++ js/object.js
@@ -1,3 +1,17 @@
+let game = document.getElementById('gameDisplayOne')
+let ctx = game.getContext('2d')
+
+console.log(getComputedStyle(game)["width"])
+console.log(game.width)
+console.log(game.height)
+
+game.setAttribute("height", getComputedStyle(game)["height"])
+game.setAttribute("width", getComputedStyle(game)["width"])
+
+// var angle = Math.random(0, 1)
+// var direction = vector(cos(angle), sin(angle))
+
+
 var playerStart = {
     x: 50,
     y: 100,
@@ -15,13 +29,15 @@ var playerStart = {
 }
 
 var enemieOneStart = {
-    x: Math.floor(Math.random(game.getAttribute('width'))),
-    y: 1,
+    x: 280,
+    y: 220,
     width: 25,
     height: 25,
     color: "red",
     alive: true,
-    velocity: 10,
+    velocity: 20,
+    // xSpeed: 3,
+    // ySpeed: 5,
     health: 50,
     render: function () {
         ctx.fillStyle = this.color
@@ -30,13 +46,49 @@ var enemieOneStart = {
 }
 
 var enemieTwoStart = {
-    x: Math.floor(Math.random(game.width)),
-    y: 1,
+    x: 250,
+    y: 175,
     width: 25,
     height: 25,
     color: "red",
     alive: true,
+    // xSpeed: 3,
+    // ySpeed: 5,
     velocity: 10,
+    health: 50,
+    render: function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+var enemieThreeStart = {
+    x: 500,
+    y: 100,
+    width: 25,
+    height: 25,
+    color: "red",
+    alive: true,
+    // xSpeed: 3,
+    // ySpeed: 5,
+    velocity: 20,
+    health: 50,
+    render: function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+var enemieFourStart = {
+    x: 475,
+    y: 300,
+    width: 25,
+    height: 25,
+    color: "red",
+    alive: true,
+    // xSpeed: 3,
+    // ySpeed: 5,
+    velocity: 15,
     health: 50,
     render: function () {
         ctx.fillStyle = this.color
@@ -56,8 +108,51 @@ var goldObjStart = {
         ctx.fillRect(this.x, this.y, this.width, this.height)
     }
 }
+var goldObjTwoStart = {
+    x: 400,
+    y: 50,
+    width: 15,
+    height: 15,
+    color: "#ffef96",
+    alive: true,
+    render: function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+var goldObjThreeStart = {
+    x: 550,
+    y: 300,
+    width: 15,
+    height: 15,
+    color: "#ffef96",
+    alive: true,
+    render: function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
+
+var goldObjFourStart = {
+    x: 700,
+    y: 400,
+    width: 15,
+    height: 15,
+    color: "#ffef96",
+    alive: true,
+    render: function () {
+        ctx.fillStyle = this.color
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+}
 
 let player = { ...playerStart }
 let enemyOne = { ...enemieOneStart }
 let enemyTwo = { ...enemieTwoStart }
+let enemyThree = { ...enemieThreeStart }
+let enemyFour = { ...enemieFourStart }
 let goldenO = { ...goldObjStart }
+let goldenOTwo = {...goldObjTwoStart}
+let goldenOThree = {...goldObjThreeStart}
+let goldenOFour = {...goldObjFourStart}
